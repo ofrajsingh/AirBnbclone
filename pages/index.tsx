cc9805e1aed830631cbc6b0ba449d1003974d9b0
ofrajsingh <rajsingh00seven@gmail.com>
2023-03-09 00:02:03 +0530

    navbar,banner and small cards section completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
-import { useEffect } from 'react';
-import ExploreData from "./Data/Explore.json"
+import exploreData from "./Data/exploreData.json";
+import SmallCard from '../components/SmallCard';
+import cardsData from './Data/cardsData.json';
+import MediumCard from '../components/MediumCard';
+
 const Home= () => {
-console.log("ex", ExploreData);
   return (
     <div>
       <Head>
@@ -16,9 +18,24 @@ console.log("ex", ExploreData);
       <Banner />
 
       <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {exploreData?.map(item=>(
+              <SmallCard key={item.img} img={item.img} location={item.location} distance={item.distance}/>
+            ))}
+          </div>
         </section>
+
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+          {cardsData?.map(item=>(
+            <MediumCard key={item.img} img={item.img} title={item.title}/>
+          ))}
+        </section>
+
+
       </main>
 
     </div>
@@ -28,13 +45,18 @@ console.log("ex", ExploreData);
 
 export default Home
 
+
+// api was not working through fetch so saved all its data in Data->Explore.json
 // export async function getStaticProps(){
 //   console.log('YOYO');
-//   const exploreData = await fetch("https://links.papareact.com/pyp").then(res=>res.json());
+  // const exploreData = await fetch("https://links.papareact.com/pyp").then(res=>res.json());
   
-//   return {
-//     props: {
-//       exploreData
-//     }
-//   }
+  // const cardsData= await fetch("https://links.papareact.com/zp1").
+  // then(res=>res.json());   
+  // return {
+  //   props: {
+  //     exploreData,
+  //     cardsData
+  //   }
+  // }
 // }
